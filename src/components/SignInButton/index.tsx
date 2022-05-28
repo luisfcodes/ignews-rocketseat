7f2ab7b4bd20c995f0e79cd27886554ba202781b
Luis Fernando <luis.silva9902@gmail.com>
2022-05-28 10:15:18 -0300

    Component creation, styling and structure organization

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.scss'
+
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+export function SignInButton(){
+
+  const isUserLoggedIn = true
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color='#04d361' />
+      Luis Fernando
+      <FiX color='#737380' className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color='#eba417' />
+      Sign In with GitHub
+    </button>
+  )
+}
