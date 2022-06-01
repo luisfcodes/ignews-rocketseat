13eca096ece0f9d62033d739ec1fb98ded7f7573
Luis Fernando <luis.silva9902@gmail.com>
2022-05-31 22:05:41 -0300

    add static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import styles from './home.module.scss'
 import { stripe } from '../services/stripe'
@@ -36,7 +36,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1KZa34Fdetgi6uVKO7JLdiz2')
 
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 //24 hours
   }
 }
