4d17e559973cc4b15641839e9303ccc5607c7683
Luis Fernando <luis.silva9902@gmail.com>
2022-06-01 20:55:27 -0300

    Create API route and add auth with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,21 @@
 import styles from './styles.module.scss'
+import { signIn, useSession, signOut } from 'next-auth/react'
 
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 export function SignInButton(){
 
-  const isUserLoggedIn = true
+  const {status, data} = useSession()
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  return status === 'authenticated' ? (
+    <button className={styles.signInButton} type="button" onClick={() => signOut()}>
       <FaGithub color='#04d361' />
-      Luis Fernando
+      {data.user.name}
       <FiX color='#737380' className={styles.closeIcon}/>
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button className={styles.signInButton} type="button" onClick={() => signIn('github')}>
       <FaGithub color='#eba417' />
       Sign In with GitHub
     </button>
