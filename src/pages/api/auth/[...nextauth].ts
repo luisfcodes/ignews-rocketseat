288794f32bee94930ae0c2a361aa16cd0c4751c6
Luis Fernando <luis.silva9902@gmail.com>
2022-06-06 21:30:22 -0300

    conection with stripe and faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   
   providers: [
@@ -13,6 +16,40 @@ export default NextAuth({
         }
       }
     }),
-    
   ],
+  callbacks: {
+    async signIn({user, account, profile, email, credentials}){
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: {
+                email: user.email
+              } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+
+      
+    }
+  }
 })
